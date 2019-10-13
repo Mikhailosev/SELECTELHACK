3cf45eb937dbbca2096f5681f1414c8814a64518
mikhailosev <mikhail.losev2@gmail.com>
2019-10-13 13:35:54 +0300

    message

--- client/src/axios-intance.js
+++ client/src/axios-intance.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 import url from "./fburl";
 const instance = axios.create({
-  baseURL: url
+  baseURL: url,
+  headers: {
+    "Access-Control-Allow-Origin": "*"
+  },
+  proxy: {
+    host: "http://185.91.55.98",
+    port: 8000
+  }
 });
 export default instance;
