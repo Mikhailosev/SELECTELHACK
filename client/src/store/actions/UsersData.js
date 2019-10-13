38cb1202a2d5b0ca26e95155cbed86d874157c5f
mikhailosev <mikhail.losev2@gmail.com>
2019-10-13 14:16:41 +0300

    front

--- client/src/store/actions/UsersData.js
+++ client/src/store/actions/UsersData.js
@@ -68,17 +68,19 @@ export const loginf = data => {
 export const login = (email, password) => {
   return (dispatch, getState) => {
     let data = { email: email, password: password };
-    dispatch(
-      axios
-        .post("http://185.91.55.98:8000/user/login/", data, {
-          headers: {
-            "Access-Control-Allow-Origin": "*"
-          }
-        })
-        .then(res => {
-          console.log(data);
-        })
-    );
+
+    axios
+      .post("http://185.91.55.98:8000/user/login/", data, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 };
 export const finish_task = () => {
